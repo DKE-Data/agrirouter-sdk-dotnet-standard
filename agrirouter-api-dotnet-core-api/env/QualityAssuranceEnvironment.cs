f4ca964003219bc9aed5217b87038699f16ba60b
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2020-01-16 12:56:00 +0100

    Documentation.

--- agrirouter-api-dotnet-core-api/env/QualityAssuranceEnvironment.cs
+++ agrirouter-api-dotnet-core-api/env/QualityAssuranceEnvironment.cs
@@ -1,8 +1,8 @@
 namespace com.dke.data.agrirouter.api.env
 {
-    /**
-     * Specific QA environment.
-     */
+    /// <summary>
+    /// Specific QA environment.
+    /// </summary>
     public class QualityAssuranceEnvironment : Environment
     {
         private static readonly string API_PREFIX = "/api/v1.0";
@@ -10,19 +10,31 @@ namespace com.dke.data.agrirouter.api.env
         private static readonly string REGISTRATION_SERVICE_URL =
             "https://agrirouter-registration-service-hubqa-eu10.cfapps.eu10.hana.ondemand.com";
 
-        private static readonly string AUTHORIZATION_SERVICE_URL = "https://agrirouter-qa.cfapps.eu10.hana.ondemand.com";
+        private static readonly string AUTHORIZATION_SERVICE_URL =
+            "https://agrirouter-qa.cfapps.eu10.hana.ondemand.com";
 
-
+        /// <summary>
+        /// API prefix for the service calls.
+        /// </summary>
+        /// <returns></returns>
         protected override string ApiPrefix()
         {
             return API_PREFIX;
         }
 
+        /// <summary>
+        /// URL for the registration service which is used for the onboard process and revoke process.
+        /// </summary>
+        /// <returns>-</returns>
         protected override string RegistrationServiceUrl()
         {
             return REGISTRATION_SERVICE_URL;
         }
 
+        /// <summary>
+        /// URL for the authorization service which is used to authorize the application to connect to the AR account.
+        /// </summary>
+        /// <returns>-</returns>
         protected override string AuthorizationServiceUrl()
         {
             return AUTHORIZATION_SERVICE_URL;
