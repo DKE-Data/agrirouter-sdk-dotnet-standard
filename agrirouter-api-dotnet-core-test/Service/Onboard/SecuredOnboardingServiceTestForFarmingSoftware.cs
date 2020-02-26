399223e297d58054f4076fdfd1f979fae5fc6a3c
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2020-02-26 17:26:40 +0100

    Fix.

--- agrirouter-api-dotnet-core-test/Service/Onboard/SecuredOnboardingServiceTestForFarmingSoftware.cs
+++ agrirouter-api-dotnet-core-test/Service/Onboard/SecuredOnboardingServiceTestForFarmingSoftware.cs
@@ -16,8 +16,6 @@ namespace Agrirouter.Api.Test.Service.Onboard
     [Collection("Integrationtest")]
     public class SecuredOnboardingServiceTestForFarmingSoftware : AbstractSecuredIntegrationTestForFarmingSoftware
     {
-        private static readonly UtcDataService UtcDataService = new UtcDataService();
-        private static readonly SignatureService SignatureService = new SignatureService();
         private static readonly HttpClient HttpClient = HttpClientFactory.HttpClient();
 
         [Fact(Skip = "Can be run to generate the authorization URL.")]
@@ -33,7 +31,7 @@ namespace Agrirouter.Api.Test.Service.Onboard
         public void GivenValidRequestTokenWhenOnboardingThenThereShouldBeAValidResponse()
         {
             var onboardingService =
-                new SecuredOnboardingService(Environment, UtcDataService, SignatureService, HttpClient);
+                new SecuredOnboardingService(Environment, HttpClient);
 
             var parameters = new OnboardParameters
             {
@@ -65,7 +63,7 @@ namespace Agrirouter.Api.Test.Service.Onboard
         public void GivenInvalidRequestTokenWhenOnboardingThenThereShouldBeAValidResponse()
         {
             var onboardingService =
-                new SecuredOnboardingService(Environment, UtcDataService, SignatureService, HttpClient);
+                new SecuredOnboardingService(Environment, HttpClient);
 
             var parameters = new OnboardParameters
             {
