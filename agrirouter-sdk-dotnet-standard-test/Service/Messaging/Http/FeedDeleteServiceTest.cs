6c03c3f7b839eebbbde23100cef6bc8404a888cd
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2021-07-21 10:19:19 +0200

    Fix message type, was "feed:confirm" and now is "feed:delete". Fix validity period assignment. Adapt test cases for the new implementation.

--- agrirouter-sdk-dotnet-standard-test/Service/Messaging/Http/FeedDeleteServiceTest.cs
+++ agrirouter-sdk-dotnet-standard-test/Service/Messaging/Http/FeedDeleteServiceTest.cs
@@ -28,7 +28,7 @@ namespace Agrirouter.Test.Service.Messaging.Http
 
         [Fact]
         public void
-            GivenExistingEndpointsWhenFeedDeleteWithoutParametersWhenPerformingQueryThenTheMessageShouldNotBeOkBecauseTheMessageIdsAreMissing()
+            GivenExistingEndpointsWhenFeedDeleteWithoutParametersWhenPerformingQueryThenTheMessageShouldNotBeOkBecauseFilterCriteriaIsMissing()
         {
             var feedDeleteService = new FeedDeleteService(new HttpMessagingService(HttpClient));
             var feedDeleteParameters = new FeedDeleteParameters
@@ -50,20 +50,20 @@ namespace Agrirouter.Test.Service.Messaging.Http
             Assert.NotNull(messages);
             Assert.NotEmpty(messages.Messages_);
             Assert.Single(messages.Messages_);
-            Assert.Equal("VAL_000017", messages.Messages_[0].MessageCode);
+            Assert.Equal("VAL_000018", messages.Messages_[0].MessageCode);
             Assert.Equal(
-                "messageIds information required to process message is missing or malformed.",
+                "Information required to process message is missing or malformed. Query does not contain any filtering criteria: messageIds, senders or validityPeriod",
                 messages.Messages_[0].Message_);
         }
 
         [Fact]
-        public void GivenExistingEndpointsWhenFeedDeleteWithUnknownMessageIdsMessageIdsThenTheResultShouldBeOk()
+        public void GivenExistingEndpointsWhenFeedDeleteWithUnknownMessageIdsMessageIdsThenTheResultShouldBeOkButThereAreNoMessagesToDelete()
         {
             var feedDeleteService = new FeedDeleteService(new HttpMessagingService(HttpClient));
             var feedDeleteParameters = new FeedDeleteParameters
             {
                 OnboardResponse = OnboardResponse,
-                MessageIds = new List<string> {Guid.NewGuid().ToString()}
+                MessageIds = new List<string> { Guid.NewGuid().ToString() }
             };
             feedDeleteService.Send(feedDeleteParameters);
 
@@ -74,7 +74,7 @@ namespace Agrirouter.Test.Service.Messaging.Http
             Assert.Single(fetch);
 
             var decodedMessage = DecodeMessageService.Decode(fetch[0].Command.Message);
-            Assert.Equal(200, decodedMessage.ResponseEnvelope.ResponseCode);
+            Assert.Equal(204, decodedMessage.ResponseEnvelope.ResponseCode);
             Assert.Equal(ResponseEnvelope.Types.ResponseBodyType.AckWithMessages,
                 decodedMessage.ResponseEnvelope.Type);
 
@@ -82,21 +82,21 @@ namespace Agrirouter.Test.Service.Messaging.Http
             Assert.NotNull(messages);
             Assert.NotEmpty(messages.Messages_);
             Assert.Single(messages.Messages_);
-            Assert.Equal("VAL_000205", messages.Messages_[0].MessageCode);
+            Assert.Equal("VAL_000208", messages.Messages_[0].MessageCode);
             Assert.Equal(
-                "Feed message cannot be found.",
+                "Feed does not contain any data to be deleted.",
                 messages.Messages_[0].Message_);
         }
 
         [Fact]
         public void
-            GivenExistingEndpointsWhenFeedDeleteWithUnknownMessageIdsSenderIdsThenTheResultShouldNotBeOkBecauseTheMessageIdsAreMissing()
+            GivenExistingEndpointsWhenFeedDeleteWithSenderIdsThenTheResultShouldNotOkButThereAreNoMessagesToDelete()
         {
             var feedDeleteService = new FeedDeleteService(new HttpMessagingService(HttpClient));
             var feedDeleteParameters = new FeedDeleteParameters
             {
                 OnboardResponse = OnboardResponse,
-                Senders = new List<string> {Guid.NewGuid().ToString()}
+                Senders = new List<string> { Guid.NewGuid().ToString() }
             };
             feedDeleteService.Send(feedDeleteParameters);
 
@@ -107,32 +107,34 @@ namespace Agrirouter.Test.Service.Messaging.Http
             Assert.Single(fetch);
 
             var decodedMessage = DecodeMessageService.Decode(fetch[0].Command.Message);
-            Assert.Equal(400, decodedMessage.ResponseEnvelope.ResponseCode);
-            Assert.Equal(ResponseEnvelope.Types.ResponseBodyType.AckWithFailure,
+            Assert.Equal(204, decodedMessage.ResponseEnvelope.ResponseCode);
+            Assert.Equal(ResponseEnvelope.Types.ResponseBodyType.AckWithMessages,
                 decodedMessage.ResponseEnvelope.Type);
 
             var messages = DecodeMessageService.Decode(decodedMessage.ResponsePayloadWrapper.Details);
             Assert.NotNull(messages);
             Assert.NotEmpty(messages.Messages_);
             Assert.Single(messages.Messages_);
-            Assert.Equal("VAL_000017", messages.Messages_[0].MessageCode);
+            Assert.Equal("VAL_000208", messages.Messages_[0].MessageCode);
             Assert.Equal(
-                "messageIds information required to process message is missing or malformed.",
+                "Feed does not contain any data to be deleted.",
                 messages.Messages_[0].Message_);
         }
 
         [Fact]
         public void
-            GivenExistingEndpointsWhenFeedDeleteWithValidityPeriodThenTheResultShouldNotBeOkBecauseTheMessageIdsAreMissing()
+            GivenExistingEndpointsWhenFeedDeleteWithFullValidityPeriodThenTheResultShouldBeOkButThereAreNoMessagesToDelete()
         {
             var feedDeleteService = new FeedDeleteService(new HttpMessagingService(HttpClient));
             var feedDeleteParameters = new FeedDeleteParameters
             {
                 OnboardResponse = OnboardResponse,
-                ValidityPeriod = new ValidityPeriod()
+                ValidityPeriod = new ValidityPeriod
+                {
+                    SentFrom = UtcDataService.Timestamp(TimestampOffset.FourWeeks),
+                    SentTo = UtcDataService.Timestamp(TimestampOffset.None)
+                }
             };
-            feedDeleteParameters.ValidityPeriod.SentTo = UtcDataService.Timestamp(TimestampOffset.None);
-            feedDeleteParameters.ValidityPeriod.SentTo = UtcDataService.Timestamp(TimestampOffset.FourWeeks);
             feedDeleteService.Send(feedDeleteParameters);
 
             Thread.Sleep(TimeSpan.FromSeconds(5));
@@ -142,18 +144,19 @@ namespace Agrirouter.Test.Service.Messaging.Http
             Assert.Single(fetch);
 
             var decodedMessage = DecodeMessageService.Decode(fetch[0].Command.Message);
-            Assert.Equal(400, decodedMessage.ResponseEnvelope.ResponseCode);
-            Assert.Equal(ResponseEnvelope.Types.ResponseBodyType.AckWithFailure,
+            Assert.Equal(204, decodedMessage.ResponseEnvelope.ResponseCode);
+            Assert.Equal(ResponseEnvelope.Types.ResponseBodyType.AckWithMessages,
                 decodedMessage.ResponseEnvelope.Type);
 
             var messages = DecodeMessageService.Decode(decodedMessage.ResponsePayloadWrapper.Details);
             Assert.NotNull(messages);
             Assert.NotEmpty(messages.Messages_);
             Assert.Single(messages.Messages_);
-            Assert.Equal("VAL_000017", messages.Messages_[0].MessageCode);
+            Assert.Equal("VAL_000208", messages.Messages_[0].MessageCode);
             Assert.Equal(
-                "messageIds information required to process message is missing or malformed.",
+                "Feed does not contain any data to be deleted.",
                 messages.Messages_[0].Message_);
         }
+
     }
 }
