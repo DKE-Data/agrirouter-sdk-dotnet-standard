e81b0606b36a102c22e0fe660f4ff765c89568ab
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2021-11-15 14:00:29 +0100

    Feature/98 add standard exception handling (#99)
    * Throw exception if we failed to send an MQTT message (#97)
    
    * Add standard system exception handling.
    
    Co-authored-by: Alexander Parshin <parshin.alexander@gmail.com>

--- agrirouter-sdk-dotnet-standard-api/Exception/CouldNotSendHttpMessageException.cs
+++ agrirouter-sdk-dotnet-standard-api/Exception/CouldNotSendHttpMessageException.cs
@@ -13,15 +13,12 @@ namespace Agrirouter.Api.Exception
         ///     Constructor.
         /// </summary>
         /// <param name="statusCode">-</param>
-        /// <param name="errorMessage">-</param>
-        public CouldNotSendHttpMessageException(HttpStatusCode statusCode, string errorMessage)
+        /// <param name="messagessage">-</param>
+        public CouldNotSendHttpMessageException(HttpStatusCode statusCode, string message) : base(message)
         {
             StatusCode = statusCode;
-            ErrorMessage = errorMessage;
         }
 
-        private HttpStatusCode StatusCode { get; }
-
-        private string ErrorMessage { get; }
+        public HttpStatusCode StatusCode { get; }
     }
 }
