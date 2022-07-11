dbf782727a15f4cb7af36c705fe14cae32dbeff8
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2022-07-11 22:47:27 +0200

    Add message parsing for the onboard response of virtual CUs. Add onboard response enhancement for virtual CUs. Add integration test for the onboard process of virtual CUs and sending of capabilities messages. Add new endpoint for integration tests. Switch applications to integration platforms. Enhance test structure.

--- agrirouter-sdk-dotnet-standard-api/Service/Messaging/Vcu/IOnboardVcuService.cs
+++ agrirouter-sdk-dotnet-standard-api/Service/Messaging/Vcu/IOnboardVcuService.cs
@@ -1,4 +1,6 @@
+using Agrirouter.Api.Dto.Onboard;
 using Agrirouter.Api.Service.Parameters;
+using Agrirouter.Cloud.Registration;
 
 namespace Agrirouter.Api.Service.Messaging.Vcu
 {
@@ -8,5 +10,13 @@ namespace Agrirouter.Api.Service.Messaging.Vcu
     public interface IOnboardVcuService : IMessagingService<OnboardVcuParameters>,
         IEncodeMessageService<OnboardVcuParameters>
     {
+        /// <summary>
+        /// Enhancing the virtual endpoint with the data from the parent to have a fully usable endpoint.
+        /// </summary>
+        /// <param name="parent">The parent.</param>
+        /// <param name="virtualCommunicationUnit">The virtual communication unit.</param>
+        /// <returns></returns>
+        OnboardResponse EnhanceVirtualCommunicationToFullyUsableOnboardResponse(OnboardResponse parent,
+            OnboardingResponse.Types.EndpointRegistrationDetails virtualCommunicationUnit);
     }
 }
