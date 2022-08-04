acf17e6bd3564db0f2116b7ba5fcc94715671ece
Frank Wiebeler <frank.wiebeler@dev4Agriculture.de>
2022-08-04 15:41:28 +0200

    +ADD: Classes for RouterDevice +ADD: Certificate Generation Service for RouterDevices

---
+++ agrirouter-sdk-dotnet-standard-api/Dto/Onboard/Inner/RouterDeviceConnectionCriteria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agrirouter.Api.Dto.Onboard.Inner
+{
+    public class RouterDeviceConnectionCriteria
+    {
+        public string ClientId { get; set; } = null!;
+
+        public string GatewayId { get; set; } = null!;
+
+        public string Host { get; set; } = null!;
+
+        public int Port { get; set; }
+    }
+
+}
---
+++ agrirouter-sdk-dotnet-standard-api/Dto/Onboard/RouterDevice.cs
@@ -0,0 +1,17 @@
+﻿using Agrirouter.Api.Dto.Onboard.Inner;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agrirouter.Api.Dto.Onboard
+{
+    public class RouterDevice
+    {
+    
+        public Authentication Authentication { get; set; } = null!;
+
+        public string DeviceAlternateId { get; set; } = null!;
+
+        public RouterDeviceConnectionCriteria ConnectionCriteria { get; set; } = null!;
+    }
+}
