b0b0d7b28f4419b462f083cd0c264c40f3da4b21
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2022-10-22 21:21:03 +0200

    Parse JSON from the AR.

--- agrirouter-sdk-dotnet-standard-api/Dto/Onboard/Inner/RouterDeviceConnectionCriteria.cs
+++ agrirouter-sdk-dotnet-standard-api/Dto/Onboard/Inner/RouterDeviceConnectionCriteria.cs
@@ -1,18 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Agrirouter.Api.Dto.Onboard.Inner
+﻿namespace Agrirouter.Api.Dto.Onboard.Inner
 {
+    /// <summary>
+    /// Connection criteria for router devices.
+    /// </summary>
     public class RouterDeviceConnectionCriteria
     {
+        /// <summary>
+        /// The client ID.
+        /// </summary>
         public string ClientId { get; set; } = null!;
 
+        /// <summary>
+        /// The gateway ID.
+        /// </summary>
         public string GatewayId { get; set; } = null!;
 
+        /// <summary>
+        /// The host.
+        /// </summary>
         public string Host { get; set; } = null!;
 
+        /// <summary>
+        /// The port.
+        /// </summary>
         public int Port { get; set; }
     }
-
 }
--- agrirouter-sdk-dotnet-standard-api/Dto/Onboard/RouterDevice.cs
+++ agrirouter-sdk-dotnet-standard-api/Dto/Onboard/RouterDevice.cs
@@ -1,17 +1,44 @@
-﻿using Agrirouter.Api.Dto.Onboard.Inner;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System;
+using Agrirouter.Api.Dto.Onboard.Inner;
+using Newtonsoft.Json;
 
 namespace Agrirouter.Api.Dto.Onboard
 {
+    /// <summary>
+    /// A router device (you can create it in the AR).
+    /// </summary>
     public class RouterDevice
     {
-    
+        /// <summary>
+        /// The authentication properties.
+        /// </summary>
         public Authentication Authentication { get; set; } = null!;
 
+        /// <summary>
+        /// The device alternate ID of the router device.
+        /// </summary>
         public string DeviceAlternateId { get; set; } = null!;
 
+        /// <summary>
+        /// The connection criteria.
+        /// </summary>
         public RouterDeviceConnectionCriteria ConnectionCriteria { get; set; } = null!;
+
+        /// <summary>
+        /// Parse a JSON string into a router device.
+        /// </summary>
+        /// <param name="json">The JSON from the AR.</param>
+        /// <returns>The domain object.</returns>
+        public static RouterDevice FromJson(string json)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<RouterDevice>(json);
+            }
+            catch (JsonReaderException e)
+            {
+                throw new ArgumentException("Please check the JSON you provided, looks like it is not valid.", e);
+            }
+        }
     }
 }
