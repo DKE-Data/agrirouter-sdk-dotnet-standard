5631a7063cd94ae761be907257cffd083ef0475c
Oliver Rahner <o.rahner@dke-data.com>
2022-12-06 08:20:23 +0100

    [WiP] Test does not yet complete

--- agrirouter-sdk-dotnet-standard-test/Integration/SendAndReceiveChunkedMessagesTest.cs
+++ agrirouter-sdk-dotnet-standard-test/Integration/SendAndReceiveChunkedMessagesTest.cs
@@ -22,14 +22,17 @@ using Xunit;
 using Agrirouter.Impl.Service.Messaging.CancellationToken;
 using Agrirouter.Request;
 using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using MQTTnet.Adapter;
 
 namespace Agrirouter.Test.Integration
 {
 
     [Collection("Integrationtest")]
-    public class SendAndReceiveChunkedMessagesTest : AbstractIntegrationTestForCommunicationUnits {
-    
-        private static int MAX_CHUNK_SIZE = 1024000;
+    public class SendAndReceiveChunkedMessagesTest : AbstractIntegrationTestForCommunicationUnits
+    {
+
+        private static int _maxChunkSize = 1024000;
 
         private static OnboardResponse Sender =>
             OnboardResponseIntegrationService.Read(Identifier.Http.CommunicationUnit.Sender);
@@ -38,15 +41,74 @@ namespace Agrirouter.Test.Integration
             OnboardResponseIntegrationService.Read(Identifier.Http.CommunicationUnit.Recipient);
 
         private static readonly HttpClient HttpClientForSender = HttpClientFactory.AuthenticatedHttpClient(Sender);
-        private static readonly HttpClient HttpClientForRecipient = HttpClientFactory.AuthenticatedHttpClient(Recipient);
 
-        
-        void givenRealMessageContentWhenSendingMessagesTheContentShouldMatchAfterReceivingAndMergingIt(ByteString messageContent, int expectedNrOfChunks) {
+        private static readonly HttpClient
+            HttpClientForRecipient = HttpClientFactory.AuthenticatedHttpClient(Recipient);
+
+        /**
+         * Cleanup before each test case. These actions are necessary because it could be the
+         * case, that there are dangling messages from former tests.
+         */
+        public SendAndReceiveChunkedMessagesTest()
+        {
+            var fetchMessageService = new FetchMessageService(HttpClientForRecipient);
+            var queryMessageHeadersService =
+                new QueryMessageHeadersService(new HttpMessagingService(HttpClientForRecipient));
+
+            //  [1] Clean the outbox of the endpoint.
+            fetchMessageService.Fetch(Recipient, new DefaultCancellationToken(3, 500));
+            //  [2] Fetch all message headers for the last 4 weeks (maximum retention time within the
+            //  agrirouter).
+            var queryMessageParameters = new QueryMessagesParameters()
+            {
+                OnboardResponse = Recipient,
+                ValidityPeriod = new ValidityPeriod()
+                {
+                    SentFrom = UtcDataService.Timestamp(TimestampOffset.FourWeeks),
+                    SentTo = UtcDataService.Timestamp(0)
+                }
+            };
+            queryMessageHeadersService.Send(queryMessageParameters);
+            Timer.WaitForTheAgrirouterToProcessTheMessage();
+            var fetchMessageResponses = fetchMessageService.Fetch(Recipient, new DefaultCancellationToken(3, 500));
+            Assert.Single(fetchMessageResponses);
+            var decodedMessage = DecodeMessageService.Decode(fetchMessageResponses[0].Command.Message);
+            Assert.Equal(ResponseEnvelope.Types.ResponseBodyType.AckForFeedHeaderList,
+                decodedMessage.ResponseEnvelope.Type);
+            var headerQueryResponse = queryMessageHeadersService.Decode(decodedMessage.ResponsePayloadWrapper.Details);
+
+            //  [3] Delete the dangling messages from the feed of the endpoint if necessary.
+            if (headerQueryResponse.QueryMetrics.TotalMessagesInQuery > 0)
+            {
+                var feedDeleteService = new FeedDeleteService(new HttpMessagingService(HttpClientForRecipient));
+                var feedDeleteParameters = new FeedDeleteParameters()
+                {
+                    OnboardResponse = Recipient,
+                    MessageIds = headerQueryResponse.Feed
+                        .SelectMany(feed => feed.Headers)
+                        .Select(header => header.MessageId)
+                        .ToList()
+                };
+                feedDeleteService.Send(feedDeleteParameters);
+                Timer.WaitForTheAgrirouterToProcessTheMessage();
+            }
+
+            //  [4] Clean the outbox of the endpoint.
+            fetchMessageService.Fetch(Recipient, new DefaultCancellationToken(3, 500));
+        }
+
+        [Fact]
+        void givenRealMessageContentWhenSendingMessagesTheContentShouldMatchAfterReceivingAndMergingIt()
+        {
+            var messageContent = ByteString.FromBase64(DataProvider.ReadBase64EncodedBigTaskData());
+            var expectedNrOfChunks = 3;
+
             this.actionsForSender(messageContent, expectedNrOfChunks);
             this.actionsForTheRecipient(messageContent, expectedNrOfChunks);
         }
-    
-        private void actionsForTheRecipient(ByteString messageContent, int expectedNrOfChunks) {
+
+        private void actionsForTheRecipient(ByteString messageContent, int expectedNrOfChunks)
+        {
             //  [1] Fetch all the messages within the feed. The number of headers should match the number of
             //  chunks sent.
             var queryMessagesService = new QueryMessagesService(new HttpMessagingService(HttpClientForRecipient));
@@ -63,32 +125,32 @@ namespace Agrirouter.Test.Integration
             //  [3] Fetch the chunks from the outbox. Since we have the same restrictions while receiving,
             //  this has to be the same number of messages as it is chunks.
             var fetchMessageService = new FetchMessageService(HttpClientForRecipient);
-            var fetchMessageResponse = fetchMessageService.Fetch(Recipient, new DefaultCancellationToken(3, 500));
-            Assert.Single(fetchMessageResponse);
-            Assert.Equal(expectedNrOfChunks, fetchMessageResponse.Count);
+            var fetchMessageResponses = fetchMessageService.Fetch(Recipient, new DefaultCancellationToken(3, 500));
+            Assert.Single(fetchMessageResponses);
+            Assert.Equal(expectedNrOfChunks, fetchMessageResponses.Count);
 
             //  [4] Check if the response from the AR only contains valid results.
-            Assert.All(fetchMessageResponse
-                .Select(response => DecodeMessageService.Decode(response.Command.Message)),
-                message => 
+            Assert.All(fetchMessageResponses
+                    .Select(response => DecodeMessageService.Decode(response.Command.Message)),
+                message =>
                     Assert.Equal(Response.ResponseEnvelope.Types.ResponseBodyType.AckForFeedMessage,
                         message.ResponseEnvelope.Type
-            ));
-            
+                    ));
+
 
             //  [5] Map the results from the query to 'real' messages within the feed and perform some
             //  assertions.
-            var feedMessages = fetchMessageResponse
+            var feedMessages = fetchMessageResponses
                 .Select(response => DecodeMessageService.Decode(response.Command.Message))
                 .Select(message => queryMessagesService.Decode(message.ResponsePayloadWrapper.Details))
                 .Select(response => response.Messages[0]);
-            
+
             Assert.Equal(expectedNrOfChunks, feedMessages.Count());
             Assert.All(feedMessages, message => Assert.NotNull(message.Header.ChunkContext));
             Assert.All(feedMessages, message => Assert.Equal(expectedNrOfChunks, message.Header.ChunkContext.Total));
             Assert.True(feedMessages.Select(message => message.Header.ChunkContext.ContextId).Distinct().Count() == 1,
                 "There should be only one chunk context ID.");
-            
+
             //  [6] Confirm the chunks to remove them from the feed.
             var messageIdsToConfirm = feedMessages.Select(message => message.Header.MessageId).ToList();
             var feedConfirmParameters = new FeedConfirmParameters()
@@ -101,15 +163,15 @@ namespace Agrirouter.Test.Integration
 
             //  [7] Fetch the response from the agrirouter after confirming the messages.
             Timer.WaitForTheAgrirouterToProcessTheMessage();
-            fetchMessageResponse = fetchMessageService.Fetch(Recipient, new DefaultCancellationToken(3, 500));
-            Assert.Single(fetchMessageResponse);
+            fetchMessageResponses = fetchMessageService.Fetch(Recipient, new DefaultCancellationToken(3, 500));
+            Assert.Single(fetchMessageResponses);
         }
-    
+
         private void actionsForSender(ByteString messageContent, int expectedNrOfChunks)
         {
             var encodeMessageService = new EncodeMessageService();
             var sendMessageService = new SendDirectMessageService(new HttpMessagingService(HttpClientForSender));
-            
+
             //  [1] Define the raw message, in this case this is the Base64 encoded message content, no
             //  chunking needed.
             var messageHeaderParameters = new MessageHeaderParameters()
@@ -117,86 +179,58 @@ namespace Agrirouter.Test.Integration
                 TechnicalMessageType = TechnicalMessageTypes.Iso11783TaskdataZip,
                 ApplicationMessageId = MessageIdService.ApplicationMessageId(),
                 Mode = RequestEnvelope.Types.Mode.Direct,
-                Recipients = 
-
+                Recipients = new List<string> {Recipient.SensorAlternateId}
             };
-            
-            MessageHeaderParameters messageHeaderParameters = new MessageHeaderParameters();
-            messageHeaderParameters.setTechnicalMessageType(ContentMessageType.ISO_11783_TASKDATA_ZIP);
-            messageHeaderParameters.setApplicationMessageId(MessageIdService.generateMessageId());
-            messageHeaderParameters.setApplicationMessageSeqNo(SequenceNumberService.generateSequenceNumberForEndpoint(onboardingResponse));
-            messageHeaderParameters.setMode(Request.RequestEnvelope.Mode.DIRECT);
-            messageHeaderParameters.setRecipients(Collections.singletonList(OnboardingResponseRepository.read(OnboardingResponseRepository.Identifier.COMMUNICATION_UNIT).getSensorAlternateId()));
-            PayloadParameters payloadParameters = new PayloadParameters();
-            payloadParameters.setValue(messageContent);
-            payloadParameters.setTypeUrl(SystemMessageType.EMPTY.getKey());
+
+            var messagePayloadParameters = new MessagePayloadParameters()
+            {
+                Value = messageContent,
+                TypeUrl = TechnicalMessageTypes.Empty
+            };
+
             //  [2] Chunk the message content using the SDK specific methods ('chunkAndEncode').
-            List<MessageParameterTuple> tuples = encodeMessageService.chunkAndBase64EncodeEachChunk(messageHeaderParameters, payloadParameters, onboardingResponse);
-            tuples.forEach(messageParameterTuple, -, Greater, Assertions.assertTrue((Objects.requireNonNull(messageParameterTuple.getPayloadParameters().getValue()).toStringUtf8().length() <= MAX_CHUNK_SIZE)));
-            List<String> encodedMessages = encodeMessageService.encode(tuples);
+            var tuples =
+                EncodeMessageService.ChunkAndBase64EncodeEachChunk(messageHeaderParameters, messagePayloadParameters);
+            Assert.All(tuples,
+                tuple => Assert.True(tuple.MessagePayloadParameters.Value.ToStringUtf8().Length <= _maxChunkSize));
+            var encodedMessages = tuples.Select(tuple =>
+                EncodeMessageService.Encode(tuple.MessageHeaderParameters, tuple.MessagePayloadParameters)).ToList();
+
             //  [3] Send the chunks to the agrirouter.
-            SendMessageParameters sendMessageParameters = new SendMessageParameters();
-            sendMessageParameters.setEncodedMessages(encodedMessages);
-            sendMessageParameters.setOnboardingResponse(onboardingResponse);
-            sendMessageService.send(sendMessageParameters);
+            var messagingParameters = new MessagingParameters()
+            {
+                OnboardResponse = Sender,
+                EncodedMessages = encodedMessages,
+                ApplicationMessageId = MessageIdService.ApplicationMessageId()
+            };
+            sendMessageService.Send(messagingParameters);
+
             //  [4] Wait for the AR to process the chunks.
-            waitForTheAgrirouterToProcessMultipleMessages();
+            Timer.WaitForTheAgrirouterToProcessTheMessage();
+
             //  [5] Check if the chunks were processed successfully.
-            FetchMessageService fetchMessageService = new FetchMessageServiceImpl();
-            Optional<List<FetchMessageResponse>> fetchMessageResponses = fetchMessageService.fetch(onboardingResponse, new DefaultCancellationToken(MAX_TRIES_BEFORE_FAILURE, DEFAULT_INTERVAL));
-            Assertions.assertTrue(fetchMessageResponses.isPresent());
-            Assertions.assertEquals(expectedNrOfChunks, fetchMessageResponses.get().size());
-            DecodeMessageService decodeMessageService = new DecodeMessageServiceImpl();
-            AtomicReference<DecodeMessageResponse> decodeMessageResponse = new AtomicReference();
-            fetchMessageResponses.get().stream().map(FetchMessageResponse: FetchMessageResponse:, :, getCommand).forEach(message, -, Greater, {, Assertions.assertNotNull(message));
-                decodeMessageResponse.set(decodeMessageService.decode(message.getMessage()));
-                Assertions.assertMatchesAny(Arrays.asList(HttpStatus.SC_OK, HttpStatus.SC_CREATED, HttpStatus.SC_NO_CONTENT), decodeMessageResponse.get().getResponseEnvelope().getResponseCode());
-            }
+            var fetchMessageService = new FetchMessageService(HttpClientForSender);
+            var fetchMessageResponses = fetchMessageService.Fetch(Sender, new DefaultCancellationToken(3, 500));
+
+            Assert.Single(fetchMessageResponses);
+            Assert.Equal(expectedNrOfChunks, fetchMessageResponses.Count);
+
+            var feedMessages = fetchMessageResponses
+                .Select(response => response.Command).ToList();
+
+            Assert.All(feedMessages, Assert.NotNull);
+            Assert.All(feedMessages, message =>
+            {
+                var decodedMessage = DecodeMessageService.Decode(message.Message);
+                Assert.Contains(decodedMessage.ResponseEnvelope.ResponseCode,
+                    new List<int>
+                    {
+                        (int) HttpStatusCode.OK,
+                        (int) HttpStatusCode.Created,
+                        (int) HttpStatusCode.NoContent
+                    });
+            });
+
         }
-        atSuppressWarnings("unused");
-        privatestaticUnknown
-    
-        [NotNull()]
-        Stream<Arguments> givenRealMessageContentWhenSendingMessagesTheContentShouldMatchAfterReceivingAndMergingIt() {
-            return Stream.of(Arguments.of(ByteString.copyFrom(ContentReader.readRawData(ContentReader.Identifier.BIG_TASK_DATA)), 3));
-        }
-    
-        [BeforeEach()]
-        [AfterEach()]
-        public void prepareTestEnvironment() {
-            FetchMessageService fetchMessageService = new FetchMessageServiceImpl();
-            OnboardingResponse recipient = OnboardingResponseRepository.read(OnboardingResponseRepository.Identifier.COMMUNICATION_UNIT);
-            MessageHeaderQueryServiceImpl messageHeaderQueryService = new MessageHeaderQueryServiceImpl(new QA());
-            //  [1] Clean the outbox of the endpoint.
-            fetchMessageService.fetch(recipient, new DefaultCancellationToken(MAX_TRIES_BEFORE_FAILURE, DEFAULT_INTERVAL));
-            //  [2] Fetch all message headers for the last 4 weeks (maximum retention time within the
-            //  agrirouter).
-            MessageQueryParameters messageQueryParameters = new MessageQueryParameters();
-            messageQueryParameters.setOnboardingResponse(recipient);
-            messageQueryParameters.setSentToInSeconds(UtcTimeService.inTheFuture(5).toEpochSecond());
-            messageQueryParameters.setSentFromInSeconds(UtcTimeService.inThePast(UtcTimeService.FOUR_WEEKS_AGO).toEpochSecond());
-            messageHeaderQueryService.send(messageQueryParameters);
-            waitForTheAgrirouterToProcessSingleMessage();
-            Optional<List<FetchMessageResponse>> fetchMessageResponses = fetchMessageService.fetch(recipient, new DefaultCancellationToken(MAX_TRIES_BEFORE_FAILURE, DEFAULT_INTERVAL));
-            Assertions.assertTrue(fetchMessageResponses.isPresent());
-            Assertions.assertEquals(1, fetchMessageResponses.get().size(), "This should be a single response.");
-            DecodeMessageService decodeMessageService = new DecodeMessageServiceImpl();
-            DecodeMessageResponse decodeMessageResponse = decodeMessageService.decode(fetchMessageResponses.get().get(0).getCommand().getMessage());
-            Assertions.assertEquals(Response.ResponseEnvelope.ResponseBodyType.ACK_FOR_FEED_HEADER_LIST, decodeMessageResponse.getResponseEnvelope().getType());
-            FeedResponse.HeaderQueryResponse headerQueryResponse = messageHeaderQueryService.decode(decodeMessageResponse.getResponsePayloadWrapper().getDetails().getValue());
-            //  [3] Delete the dangling messages from the feed of the endpoint if necessary.
-            if ((headerQueryResponse.getQueryMetrics().getTotalMessagesInQuery() > 0)) {
-                DeleteMessageServiceImpl deleteMessageService = new DeleteMessageServiceImpl();
-                DeleteMessageParameters deleteMessageParameters = new DeleteMessageParameters();
-                deleteMessageParameters.setOnboardingResponse(recipient);
-                List<String> messageIds = headerQueryResponse.getFeedList().stream().map(FeedResponse.HeaderQueryResponse.Feed, :, :, getHeadersList).flatMap(Collection: Collection:, :, stream).map(FeedResponse.HeaderQueryResponse.Header, :, :, getMessageId).collect(Collectors.toList());
-                deleteMessageParameters.setMessageIds(messageIds);
-                deleteMessageService.send(deleteMessageParameters);
-                waitForTheAgrirouterToProcessSingleMessage();
-                fetchMessageService.fetch(recipient, new DefaultCancellationToken(MAX_TRIES_BEFORE_FAILURE, DEFAULT_INTERVAL));
-            }
-        
-            //  [4] Clean the outbox of the endpoint.
-            fetchMessageService.fetch(recipient, new DefaultCancellationToken(MAX_TRIES_BEFORE_FAILURE, DEFAULT_INTERVAL));
-        }
+    }
 }
